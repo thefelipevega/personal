e2edc0360e96a7706874f3d731d352b588f704b6
Felipe Vega <felipevega@hey.com>
2022-09-14 21:14:36 -0700

    add basic info section

--- source/index.blade.php
+++ source/index.blade.php
@@ -26,4 +26,21 @@
     </a>
   </div>
 </div>
+<div class="prose prose-invert">
+  <p>
+    I never know how to start these things, so let's go over the
+    <span class="underline decoration-sky-400 decoration-2 underline-offset-4">basics</span>.
+  </p>
+  <ul>
+    <li>I'm a product manager and web developer based in Los Angeles California.</li>
+    <li>I live with my beautiful girlfriend and our two dogs Snowbear and Luna.</li>
+    <li>Even though I live in sunny-LA, I love the rain!</li>
+    <li>I dabble in writing and world-building (for D&D and short stories)</li>
+    <li>
+      I have a lot of opinions on product design, project management, software development, optimal room temperature
+      (68ºF fyi), and much more!
+    </li>
+  </ul>
+  <p>Got it? Good! Now let's dive into the nitty-gritty.</p>
+</div>
 @endsection
