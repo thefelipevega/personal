517066f39869571ab34fb798eb0ee1dbd5e2096d
Felipe Vega <felipevega@hey.com>
2022-09-16 23:38:57 -0700

    simplify footer

--- source/_components/footer.blade.php
+++ source/_components/footer.blade.php
@@ -1,19 +1,3 @@
-<footer class="pb-8">
-  <div
-    class="mx-auto flex max-w-7xl flex-col items-center space-y-8 overflow-hidden md:flex-row md:justify-between md:space-y-0">
-    <nav class="-mx-5 -my-2 flex flex-wrap justify-center" aria-label="Footer">
-      <div class="px-5 py-2">
-        <a href="#" class="text-base hover:text-sky-400">About</a>
-      </div>
-      <div class="px-5 py-2">
-        <a href="#" class="text-base hover:text-sky-400">Writing</a>
-      </div>
-      <div class="px-5 py-2">
-        <a href="#" class="text-base hover:text-sky-400">Uses</a>
-      </div>
-    </nav>
-    <p class="text-center text-base">
-      &copy; {{ Carbon\Carbon::now()->year }} <span class="text-sky-400">Felipe Vega</span>. All rights reserved.
-    </p>
-  </div>
+<footer class="flex flex-col items-center pb-8 text-sm text-zinc-400">
+  <p>&copy; {{ Carbon\Carbon::now()->year }} Felipe Vega. All rights reserved.</p>
 </footer>
