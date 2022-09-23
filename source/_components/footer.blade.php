7d07bd951ffd5816908e2c4f5fcba4e7a731766d
Felipe Vega <felipevega@hey.com>
2022-09-23 16:27:56 -0700

    fix spacing for header and footer

--- source/_components/footer.blade.php
+++ source/_components/footer.blade.php
@@ -1,3 +1,3 @@
-<footer class="flex flex-col items-center pb-8 text-sm text-zinc-400">
+<footer class="flex flex-col items-center py-4 text-sm text-zinc-400">
   <p>&copy; {{ Carbon\Carbon::now()->year }} Felipe Vega. All rights reserved.</p>
 </footer>
