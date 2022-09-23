0d75637c939b79e47b272d3e79b23b842413343b
Felipe Vega <felipevega@hey.com>
2022-09-23 16:36:05 -0700

    segment index sections into components

---
+++ source/_components/home/about.blade.php
@@ -0,0 +1,16 @@
+<div id="about" class="prose text-inherit marker:text-sky-400 sm:prose-lg">
+  <p>
+    I never know how to start these things, so let's go over the
+    <span class="underline decoration-sky-400 decoration-2 underline-offset-4">basics</span>.
+  </p>
+  <ul>
+    <li>I'm a product manager and web developer based in Los Angeles California.</li>
+    <li>I live with my beautiful girlfriend and our two dogs Snowbear and Luna.</li>
+    <li>Even though I live in sunny-LA, I love the rain!</li>
+    <li>I dabble in writing and world-building (for D&D and short stories)</li>
+    <li>
+      I have a lot of opinions on product design, project management, software development, optimal room temperature
+      (68ºF fyi)...I could go on.
+    </li>
+  </ul>
+</div>
---
+++ source/_components/home/work.blade.php
@@ -0,0 +1,82 @@
+<div id="work" class="not-prose space-y-4 rounded-xl border-2 border-zinc-800 p-4 font-medium">
+  <div class="flex items-center justify-between">
+    <h2 class="flex items-center text-xl font-semibold">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="ml-1 h-8 w-8">
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008z" />
+      </svg>
+      <span class="ml-3">Work</span>
+    </h2>
+    <a
+      class="group inline-flex items-center justify-center gap-1 rounded-md bg-zinc-800/50 py-2 px-3 text-sm font-medium text-zinc-300 outline-offset-2 transition hover:bg-zinc-800 hover:text-zinc-50 active:bg-zinc-800/50 active:text-zinc-50/70"
+      href="/#">
+      View Resume
+      <svg
+        viewBox="0 0 24 24"
+        fill="none"
+        aria-hidden="true"
+        class="h-5 w-5 stroke-zinc-400 transition group-hover:stroke-zinc-50 group-active:stroke-zinc-50">
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m.75 12l3 3m0 0l3-3m-3 3v-6m-1.5-9H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
+      </svg>
+    </a>
+  </div>
+  <div class="flex flex-col pt-2 pb-4">
+    <div class="relative pb-8">
+      <span class="absolute left-5 -ml-px h-full w-0.5 border border-dashed border-sky-400" aria-hidden="true"></span>
+      <div class="relative flex items-center space-x-2">
+        <div class="flex-shrink-0 rounded-full ring-2 ring-sky-400 ring-offset-2 ring-offset-zinc-900">
+          <img
+            alt="FlashSpread Logo"
+            src="/assets/images/flashspread-logo.jpeg"
+            class="h-10 w-10 rounded-full object-cover" />
+        </div>
+        <div class="grid w-full grid-cols-2">
+          <p class="col-span-2">FlashSpread</p>
+          <span class="text-xs text-zinc-400">Product Manager</span>
+          <span class="ml-auto text-xs text-zinc-400">2022 — Present</span>
+        </div>
+      </div>
+    </div>
+    <div class="relative pb-8">
+      <span class="absolute left-5 -ml-px h-full w-0.5 border border-dashed border-zinc-700" aria-hidden="true"></span>
+      <div class="relative flex items-start space-x-2">
+        <div class="flex-shrink-0 rounded-full ring-2 ring-zinc-700 ring-offset-2 ring-offset-zinc-900">
+          <img
+            alt="BeSmartee Logo"
+            src="/assets/images/besmartee-logo.png"
+            class="object-over h-10 w-10 rounded-full" />
+        </div>
+        <div class="grid w-full grid-cols-2">
+          <p class="col-span-2">BeSmartee</p>
+          <span class="text-xs text-zinc-400">Product Manager</span>
+          <span class="ml-auto mb-2 text-xs text-zinc-400">2021 — Present</span>
+          <span class="text-xs text-zinc-400">Lead Developer</span>
+          <span class="ml-auto mb-2 text-xs text-zinc-400">2019 — 2021</span>
+          <span class="text-xs text-zinc-400">Web Developer</span>
+          <span class="ml-auto text-xs text-zinc-400">2017 — 2019</span>
+        </div>
+      </div>
+    </div>
+    <div class="relative flex items-center space-x-2">
+      <div class="flex-shrink-0 rounded-full ring-2 ring-zinc-700 ring-offset-2 ring-offset-zinc-900">
+        <img alt="Felipe Vega" src="/assets/images/headshot-angled.jpg" class="h-10 w-10 rounded-full object-cover" />
+      </div>
+      <div class="grid w-full grid-cols-2">
+        <p class="col-span-2">Freelance</p>
+        <span class="text-xs text-zinc-400">Web Developer</span>
+        <span class="ml-auto text-xs text-zinc-400">2014 — 2017</span>
+      </div>
+    </div>
+  </div>
+</div>
