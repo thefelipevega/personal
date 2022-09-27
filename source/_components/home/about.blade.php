8231b673a468fe26baa7f793495e3ee4e4219c3d
Felipe Vega <felipevega@hey.com>
2022-09-26 19:11:53 -0700

    improve about section and add family image

--- source/_components/home/about.blade.php
+++ source/_components/home/about.blade.php
@@ -1,20 +1,55 @@
 <div id="about" {{ $attributes->
-  merge(['class' => "prose text-inherit marker:text-sky-400 sm:prose-lg lg:prose-xl"]) }}>
-  <h2 class="mb-4 w-full text-4xl font-medium text-white sm:text-5xl lg:text-5xl">
+  merge(['class'=>"sm:px-4 mx-auto prose prose-lg text-inherit marker:text-sky-400 sm:prose-lg sm:max-w-none lg:p-0"])
+  }}>
+  <h2 class="mb-4 w-full text-4xl font-medium text-white sm:text-5xl lg:mb-6">
     Product manager, web developer, & bag of opinions.
   </h2>
   <p>
-    I never know how to start these things, so let's go over the
+    I never know how to fill this area up, so let's go over the
     <span class="underline decoration-sky-400 decoration-2 underline-offset-4">basics</span>.
   </p>
-  <ul>
+  <ul class="list-inside px-0">
     <li>I'm a product manager and web developer based in Los Angeles California.</li>
-    <li>I live with my beautiful girlfriend and our two dogs Snowbear and Luna.</li>
-    <li>Even though I live in sunny-LA, I love the rain!</li>
-    <li>I dabble in writing and world-building (for D&D and short stories)</li>
+    <li>
+      I live with my beautiful girlfriend and our two dogs <span class="text-sky-400">Snowbear</span> and
+      <span class="text-pink-400">Luna</span>.
+    </li>
+    <li>Even though I live in sunny Los Angeles, I hate the heat and I love the rain!</li>
+    <li>
+      I write and world-build for my custom Dungeons and Dragons setting (#foreverDM) and have plans for some other
+      short stories in the future🤞🏻
+    </li>
     <li>
       I have a lot of opinions on product design, project management, software development, optimal room temperature
-      (68ºF fyi)...I could go on.
+      (68ºF fyi)...
     </li>
   </ul>
+  <p class="text-center">I could go on 😅, but instead here's a picture of me and my family.</p>
+  <figure class="mx-auto max-w-sm rotate-2">
+    <img class="rounded-xl" src="/assets/images/family.jpeg" alt="Myself, Sandra, Luna and Snowbear" />
+    <figcaption class="-mt-px flex">
+      <span class="flex w-1/2 items-baseline justify-center text-pink-400">
+        <svg
+          class="h-8 w-8 stroke-current stroke-2"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke-linecap="round"
+          stroke-linejoin="round">
+          <path d="M18 18h-6a3 3 0 0 1 -3 -3v-10l-5 5m10 0l-5 -5" />
+        </svg>
+        <span class="font-semibold">Luna</span>
+      </span>
+      <span class="flex w-1/2 items-baseline justify-center text-sky-400">
+        <span class="font-semibold">'Snowy'</span>
+        <svg
+          class="h-8 w-8 stroke-current stroke-2"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke-linecap="round"
+          stroke-linejoin="round">
+          <path d="M6 18h6a3 3 0 0 0 3 -3v-10l-5 5m10 0l-5 -5" />
+        </svg>
+      </span>
+    </figcaption>
+  </figure>
 </div>
