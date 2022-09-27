63678b60c68087ad89664aa3d6addcc1cab32a47
Felipe Vega <felipevega@hey.com>
2022-09-26 19:20:21 -0700

    fix missing footer gap on scroll

--- source/_components/footer.blade.php
+++ source/_components/footer.blade.php
@@ -1,3 +1,3 @@
-<footer class="flex flex-col items-center text-sm text-zinc-400">
+<footer class="flex flex-col items-center pb-4 text-sm text-zinc-400 lg:pb-0">
   <p>&copy; {{ Carbon\Carbon::now()->year }} Felipe Vega. All rights reserved.</p>
 </footer>
