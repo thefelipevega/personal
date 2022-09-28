73f4e98f1e0857ce65bdb86ba5c892fbb705771f
Felipe Vega <felipevega@hey.com>
2022-09-27 18:24:55 -0700

    fix favicon format

--- source/_layouts/main.blade.php
+++ source/_layouts/main.blade.php
@@ -6,9 +6,8 @@
     <title>{{ $page->title }}</title>
     <link rel="canonical" href="{{ $page->getUrl() }}" />
     <meta name="description" content="{{ $page->description }}" />
-    <link
-      rel="icon"
-      href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👨🏻‍💻</text></svg>" />
+    <link rel="shortcut icon" type="image/png" sizes="32x32" href="assets/images/favicon-32x32.png" />
+    <link rel="shortcut icon" type="image/png" sizes="16x16" href="assets/images/favicon-16x16.png" />
     <link href="https://fonts.googleapis.com/css2?family=Cabin:wght@400;500;600;700" rel="stylesheet" />
     <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}" />
     <script defer src="{{ mix('js/main.js', 'assets/build') }}"></script>
