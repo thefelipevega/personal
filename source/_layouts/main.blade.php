364893df7603d184c714cfc840372f3fc721fe70
Felipe Vega <felipevega@hey.com>
2022-09-27 17:50:01 -0700

    simplify favicon setup

--- source/_layouts/main.blade.php
+++ source/_layouts/main.blade.php
@@ -6,10 +6,9 @@
     <title>{{ $page->title }}</title>
     <link rel="canonical" href="{{ $page->getUrl() }}" />
     <meta name="description" content="{{ $page->description }}" />
-    <link rel="apple-touch-icon" sizes="180x180" href="/assets/images/apple-touch-icon.png" />
-    <link rel="icon" type="image/png" sizes="32x32" href="/assets/images/favicon-32x32.png" />
-    <link rel="icon" type="image/png" sizes="16x16" href="/assets/images/favicon-16x16.png" />
-    <link rel="manifest" href="/site.webmanifest" />
+    <link
+      rel="icon"
+      href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👨🏻‍💻</text></svg>" />
     <link href="https://fonts.googleapis.com/css2?family=Cabin:wght@400;500;600;700" rel="stylesheet" />
     <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}" />
     <script defer src="{{ mix('js/main.js', 'assets/build') }}"></script>
